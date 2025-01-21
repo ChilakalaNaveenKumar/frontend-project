64b7c4deeb836e4fe0059c0f4b7994d59ffeabbb
Naveen Chilakala <nchilakala.c@lifevantage.com>
2025-01-20 19:43:13 -0500

    push dist changes

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/frontend-project/' : '/',
   configureWebpack: {
     resolve: {
       alias: {
